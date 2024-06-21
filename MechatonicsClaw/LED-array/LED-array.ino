3222c84b8cbfaf700d297854cfa1bc4c081500bb
Liam W <158523770+WuMail24@users.noreply.github.com>
2024-06-21 14:02:48 +1000

    cpp and h

--- MechatonicsClaw/LED-array/LED-array.ino
+++ MechatonicsClaw/LED-array/LED-array.ino
@@ -2,13 +2,7 @@
 
 
 void setup() {
-  Serial.begin(9600);
-  matrix.begin();
 
-  matrix.beginDraw();
-  matrix.stroke(0xFFFFFFFF);
-  matrix.textScrollSpeed(50);
-  const char text[] = "v0.0.001";
 }
 
 void loop() {
