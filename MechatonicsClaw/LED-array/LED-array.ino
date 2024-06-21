23c505305303a592b17a56c36ecd911ad7429389
Liam W <158523770+WuMail24@users.noreply.github.com>
2024-06-21 14:14:54 +1000

    version number led array done

--- MechatonicsClaw/LED-array/LED-array.ino
+++ MechatonicsClaw/LED-array/LED-array.ino
@@ -1,8 +1,17 @@
+#include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
 
+ArduinoLEDMatrix matrix;
 
 void setup() {
-
+  Serial.begin(9600);
+   matrix.begin();
+   matrix.textFont(Font_5x7);
+   matrix.textScrollSpeed(100);
+   matrix.stroke(0xFF, 0, 0);
+   matrix.beginText(0, 1, 0xFF, 0, 0);
+   matrix.print("V00.00.01");
+   matrix.endText(SCROLL_LEFT);
 }
 
 void loop() {
