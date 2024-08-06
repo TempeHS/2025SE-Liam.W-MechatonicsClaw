3caf1fa11e457bac1cdb261f64419587690450d5
Liam W <158523770+WuMail24@users.noreply.github.com>
2024-08-07 09:40:35 +1000

    myClaw stuff

---
+++ MechatonicsClaw/myClaw/myClaw.h
@@ -0,0 +1,22 @@
+#ifndef myClaw_H
+#define myClaw_H
+#include <Arduino.h>
+//inherit from other classes
+#include "myServo.h"
+
+class myClaw : public myServo
+{
+private:
+
+public:
+  myClaw() {} //don't use
+  myClaw();
+  virtual void init();
+  virtual void writevalue();
+  void open();
+  void close();
+  void blockclose();
+};
+
+
+#endif 
