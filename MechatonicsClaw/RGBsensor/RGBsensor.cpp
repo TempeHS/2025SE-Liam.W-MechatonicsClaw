6ed610089cd317da67a0f68e0939c36d77699792
Liam W <158523770+WuMail24@users.noreply.github.com>
2024-08-09 11:01:51 +1000

    finish intergrating example next

--- MechatonicsClaw/RGBsensor/RGBsensor.cpp
+++ MechatonicsClaw/RGBsensor/RGBsensor.cpp
@@ -1,19 +1,29 @@
 #include "RGBsensor.h" 
 #include <Arduino.h>
-#include <107-Arduino-TCS3472.h>
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+
+ArduinoTCS3472 sensor;
+Adafruit_TCS34725 mySensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 RGBsensor::RGBsensor(byte pin) {
   _pin = pin;
 }
 
 void RGBsensor::RGBinit() {
-  pinMode(_pin, INPUT);
-  readstate();
 }
-void RGBsensor::readstate() {
-  int colour = analogRead(_pin);
-}
+void RGBsensor::RGBreadcolour() {}
 
-void RGBsensor::RGBreadcolour() {
-  if colour == 
+void RGBsensor::printcolour() {
+  float red, green, blue;
+
+  delay(60);  // takes 50ms to read
+
+  tcs.getRGB(&red, &green, &blue);
+
+  Serial.print("R:\t"); Serial.print(int(red)); 
+  Serial.print("\tG:\t"); Serial.print(int(green)); 
+  Serial.print("\tB:\t"); Serial.print(int(blue));
+
+  Serial.print("\n");
 }
--- MechatonicsClaw/RGBsensor/RGBsensor.ino
+++ MechatonicsClaw/RGBsensor/RGBsensor.ino
@@ -1,10 +1,34 @@
 #include "RGBsensor.h" 
 
+RGBsensor mySensor;
+
+byte gammatable[256];
+
+Adafruit_TCS34725 mySensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
 void setup() {
   Serial.begin(9600);
-  
+
+  if (mySensor.begin()) {
+    //Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+
+  // Convert RGB colors to what humans see
+  for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+
+    gammatable[i] = 255 - x;
+
+  }
 }
 
+
 void loop() {
-  
+  mySensor.printcolour()
 }
