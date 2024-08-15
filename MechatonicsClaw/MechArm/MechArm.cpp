141ea818d81397e95eddb8414e28a31cf0025039
Liam W <158523770+WuMail24@users.noreply.github.com>
2024-08-15 13:22:10 +1000

    make sure you define constant variables in header file

--- MechatonicsClaw/MechArm/MechArm.cpp
+++ MechatonicsClaw/MechArm/MechArm.cpp
@@ -6,3 +6,13 @@
 #include "myServo.h"
 #include "myClaw.h"
 
+MechArm::MechArm (LEDArray& LEDArray, RGBsensor& RGBsensor, myBase& myBase, myClaw& myClaw, myServo& myServoUpper, myServo& myServoLower)
+  : LEDArray(LEDArray), RGBsensor(RGBsensor), myBase(myBase), myClaw(myClaw), myServo(myServoUpper), myServo(myServoLower) {}
+
+void MechArm::MechArm.init() {
+  LEDArray.init();
+  RGBsensor.RGBinit();
+  myBase.init();
+  myClaw.init();
+  myServo.init();
+}
