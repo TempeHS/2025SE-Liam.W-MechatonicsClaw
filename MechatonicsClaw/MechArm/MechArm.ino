59eb4d2553a4b871314a2d870e1b2edf08eb4d17
Liam W <158523770+WuMail24@users.noreply.github.com>
2024-08-16 11:02:10 +1000

    pin setup

--- MechatonicsClaw/MechArm/MechArm.ino
+++ MechatonicsClaw/MechArm/MechArm.ino
@@ -6,13 +6,23 @@
 #include "myClaw.h"
 //variables
 static const char* VerNum = "V00.00.21";
+static unsigned int basepin = 9;
+static unsigned int lowerpin = 10;
+static unsigned int upperpin = 11;
+static unsigned int clawpin = 6;
+//unused 
+static unsigned int basestate = 0;
+static unsigned int clawstate = 0;
+static unsigned int upperstate = 0;
+static unsigned int lowerstate = 0;
 //object setup
 LEDArray array(VerNum);
 RGBsensor rgbsensor();
 myBase base(basepin, basestate);
 myClaw claw(clawpin, clawstate);
 myServo myservoupper(upperpin, upperstate);
-MechArm arm();
+myServo myservolower(lowerpin, lowerstate);
+MechArm arm(array, rgbsensor, base, claw, myservoupper, myservolower);
 
 
 void setup() {
